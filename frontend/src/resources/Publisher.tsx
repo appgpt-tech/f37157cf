c6b79f81c620271c2f7c5c7892d19dbee521bfe4
11ai <124818466+11ai@users.noreply.github.com>
2024-01-16 23:40:14 +0200

    created on Tue Jan 16 2024 23:39:57 GMT+0200 (Eastern European Standard Time)

---
+++ frontend/src/resources/Publisher.tsx
@@ -0,0 +1,89 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const PublisherTitle = () => {
+  const record = useRecordContext();
+  return <span>Publisher {record ? `"${ record.Name }"` : ""}</span>;
+};
+
+export const PublisherList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Name" />
+<TextField source="Location" />
+<TextField source="Contactinformation" />
+<NumberField source="Id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const PublisherEdit = () => (
+                    <Edit title={<PublisherTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Name"   />
+<TextInput source="Location"   />
+<TextInput source="Contactinformation"   />
+<NumberInput source="Id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const PublisherCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Name"   />
+<TextInput source="Location"   />
+<TextInput source="Contactinformation"   />
+<NumberInput source="Id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+
+    ];
+
+
