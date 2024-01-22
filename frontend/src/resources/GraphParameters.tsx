3394b8a5760e0e40d13e606e0395c2f5a5d4cc07
11ai <124818466+11ai@users.noreply.github.com>
2024-01-22 22:39:05 +0200

    created on Mon Jan 22 2024 20:38:51 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/GraphParameters.tsx
@@ -0,0 +1,85 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const GraphParametersTitle = () => {
+  const record = useRecordContext();
+  return <span>GraphParameters {record ? `"${ record.Daysofweek }"` : ""}</span>;
+};
+
+export const GraphParametersList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Daysofweek" />
+<TextField source="Drinkingamount" />
+<NumberField source="Id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const GraphParametersEdit = () => (
+                    <Edit title={<GraphParametersTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Daysofweek"   />
+<TextInput source="Drinkingamount"   />
+<NumberInput source="Id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const GraphParametersCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Daysofweek"   />
+<TextInput source="Drinkingamount"   />
+<NumberInput source="Id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+
+    ];
+
+
