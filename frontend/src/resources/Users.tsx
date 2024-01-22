ec24b91ab11c90d66b21f71563b261f8ed26599c
11ai <124818466+11ai@users.noreply.github.com>
2024-01-22 22:39:09 +0200

    created on Mon Jan 22 2024 20:38:51 GMT+0000 (Coordinated Universal Time)

---
+++ frontend/src/resources/Users.tsx
@@ -0,0 +1,89 @@
+import {
+  Datagrid,
+  List,
+  EditButton,
+  Edit,
+  SimpleForm,
+  Create,
+  SelectColumnsButton,
+  DatagridConfigurable,
+  TopToolbar,
+  CreateButton,
+  ExportButton,
+  FilterButton,
+  //Field controls
+  BooleanField,
+  DateField,
+  EmailField,
+  ImageField,
+  NumberField,
+  ReferenceField,
+  TextField,
+  UrlField,
+  //Input controls
+  BooleanInput,
+  DateInput,
+  //EmailInput,
+  ImageInput,
+  NumberInput,
+  ReferenceInput,
+  TextInput,
+  //UrlInput,
+} from "react-admin";
+import { useRecordContext } from "react-admin";
+const ListActions = () => (
+    <TopToolbar>
+        <FilterButton />
+        <CreateButton />
+        <ExportButton />
+        <SelectColumnsButton />
+    </TopToolbar>
+);
+const UsersTitle = () => {
+  const record = useRecordContext();
+  return <span>Users {record ? `"${ record.Name }"` : ""}</span>;
+};
+
+export const UsersList = () => (
+      <List actions={<ListActions  />} filters={ResourceFilters} >
+        <DatagridConfigurable>
+          <TextField source="Name" />
+<TextField source="Surname" />
+<TextField source="Consumptionofalcohol" />
+<NumberField source="Id" /><EditButton />
+
+        </DatagridConfigurable>
+      </List>
+      );
+
+export const UsersEdit = () => (
+                    <Edit title={<UsersTitle />}>
+                      <SimpleForm>
+                          <TextInput source="Name"   />
+<TextInput source="Surname"   />
+<TextInput source="Consumptionofalcohol"   />
+<NumberInput source="Id"   disabled/>
+                      </SimpleForm>
+                    </Edit>
+                  );
+
+export const UsersCreate = () => (
+                                  <Create>
+                                    <SimpleForm>
+                                        <TextInput source="Name"   />
+<TextInput source="Surname"   />
+<TextInput source="Consumptionofalcohol"   />
+<NumberInput source="Id"   disabled/>
+                                    </SimpleForm>
+                                  </Create>
+                                );
+
+const ResourceFilters = [
+      <TextInput source="q" label="Search" alwaysOn />,
+,
+,
+,
+
+    ];
+
+
