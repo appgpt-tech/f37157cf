0ca90b03ce16d889bd3a5f0632158bcf764b2557
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 21:16:31 +0200

    edited on Wed Jan 24 2024 19:16:20 GMT+0000 (Coordinated Universal Time)

--- frontend/src/i18nProvider.ts
+++ frontend/src/i18nProvider.ts
@@ -3,7 +3,7 @@
     import polyglotI18nProvider from "ra-i18n-polyglot";
     import enOriginal from 'ra-language-english';
 
-    const enResources = { resources: {"Users":{"name":"users","fields":{"Name":"Name","Surname":"Surname","Consumptionofalcohol":"Consumptionofalcohol","Id":"Id"}},"GraphParameters":{"name":"graphParameters","fields":{"Daysofweek":"Daysofweek","Drinkingamount":"Drinkingamount","Id":"Id"}}}};
+    const enResources = { resources: {"Books":{"name":"books","fields":{"Title":"Title","Releasedate":"Releasedate","Id":"Id"}}}};
 
 
     const en = mergeTranslations(enOriginal,enResources);
