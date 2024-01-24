51d6f5442ecf416513b43c5f92a0fdea8615b63c
11ai <124818466+11ai@users.noreply.github.com>
2024-01-24 21:16:33 +0200

    edited on Wed Jan 24 2024 19:16:20 GMT+0000 (Coordinated Universal Time)

--- frontend/src/dashboard.tsx
+++ frontend/src/dashboard.tsx
@@ -1,7 +1,7 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
     export const Dashboard = () => (
       <Card>
-      <CardHeader title= "y no gradient" />
-      <CardContent> an application that makes gradients but doent sshow em < /CardContent>
+      <CardHeader title= "libby" />
+      <CardContent> Book Archive < /CardContent>
       </Card>
     );
