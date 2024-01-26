cce23ed08db23620e9f31f76b364fe1f53085814
11ai <124818466+11ai@users.noreply.github.com>
2024-01-26 21:52:58 +0200

    edited on Fri Jan 26 2024 21:52:51 GMT+0200 (Eastern European Standard Time)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,12 +8,8 @@ import { authProvider, apInitialize } from "./authProvider";
 import { i18nProvider } from "./i18nProvider";
 import LoginPage, { Login } from "./Login";
 import data from "./data";
-import { BooksList, BooksCreate, BooksEdit} from "./resources/Books";
-import { AuthorsList, AuthorsCreate, AuthorsEdit} from "./resources/Authors";
-import { GenresList, GenresCreate, GenresEdit} from "./resources/Genres";
-import BooksIcon from "@mui/icons-material/LibraryBooks";
-import AuthorsIcon from "@mui/icons-material/Person";
-import GenresIcon from "@mui/icons-material/Category"; 
+
+ 
 // SUPERTOKENS
 import React from "react";
 import SuperTokens, {
@@ -75,24 +71,7 @@ const App = () => (
         dashboard={Dashboard}
         
       >
-    <Resource name="Books" options={{label:"books"}} 
-list={BooksList}
-create={BooksCreate}
-edit={BooksEdit}
-recordRepresentation="title"
-icon={BooksIcon}/>
-<Resource name="Authors" options={{label:"authors"}} 
-list={AuthorsList}
-create={AuthorsCreate}
-edit={AuthorsEdit}
-recordRepresentation="name"
-icon={AuthorsIcon}/>
-<Resource name="Genres" options={{label:"genres"}} 
-list={GenresList}
-create={GenresCreate}
-edit={GenresEdit}
-recordRepresentation="category"
-icon={GenresIcon}/>
+    
     <CustomRoutes noLayout>
       {/*This renders the login UI on the /auth route*/}
       {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
